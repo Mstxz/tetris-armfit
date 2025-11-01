45fdf47ad5a5206e160f70524450f7e35cd768fa
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2025-11-02 03:58:49 +0700

    gyro position

--- Tetris/Tetris.ino
+++ Tetris/Tetris.ino
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include <LedControl.h>
 #include <type_traits>
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
 #include "TetrisGame.hpp"
 
 // 4 MAX7219 modules → 32x8 matrix
@@ -28,20 +32,20 @@ void loop() {
 	t_event ev = IDLE;
 
 	// TODO: Change input into using mpu6050
-	/*
+
 	if (millis() - lastInput > INPUT_DELAY)
 	{
-		if (x_val < -100)
-			game.KeyEvent(MOVE_LEFT);
-		else if (x_val > 100)
-			game.KeyEvent(MOVE_RIGHT);
-		else if (y_val > 150 && millis() - game.getShapeSpawnTime() > POLLING_RATE)
+		if (y_val < -15) 								//this will be rotate left (y is below -15) 					/*x_val < -100*/
+			game.KeyEvent(MOVE_LEFT); 
+		else if (y_val > 15) 						//this will be rotate right (y is above 15)						/*x_val > 100*/
+			game.KeyEvent(MOVE_RIGHT); 
+		else if (x_val < -15) 					//this will be move up (x is below -15)								/*y_val > 150 && millis() - game.getShapeSpawnTime() > POLLING_RATE/*/
 			game.KeyEvent(MOVE_DOWNWARD);
-		else if (digitalRead(2) == LOW)
+		else if (x_val > 15) 						//this will be move up (x is above 15)								/*digitalRead(2) == LOW*/
 			game.KeyEvent(ROTATE_RIGHT);
 		lastInput = millis();
 	}
-	*/
+
 
 	if (ev != IDLE) game.KeyEvent(ev);
 
